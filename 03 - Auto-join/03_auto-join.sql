7cf336ade6b5b887cb78e329691791455e1b1eba
Nabil El Mahfoudi <0105naelmahfoudi@student.epfc.eu>
2026-06-19 23:47:43 +0200

    feat(auto-join): remaining exercices + docs(auto-join): professors corrections

--- 03 - Auto-join/03_auto-join.sql
+++ 03 - Auto-join/03_auto-join.sql
@@ -23,3 +23,28 @@ SELECT DISTINCT s.SNAME
 FROM s
     JOIN spj on s.ID_S = spj.ID_S
 WHERE YEAR(spj.DATE_DERNIERE_LIVRAISON) BETWEEN 2015 and 2019;
+
+-- exercice 6
+SELECT s.ID_S, p.ID_P, j.ID_J
+FROM s,p,j
+WHERE s.CITY <> p.CITY
+   OR p.CITY <> j.CITY;
+
+-- exercice 7
+SELECT DISTINCT spj1.ID_P, spj2.ID_P, spj1.ID_S
+FROM SPJ spj1, SPJ spj2
+WHERE spj1.ID_S = spj2.ID_S
+  AND spj1.ID_P > spj2.ID_P;
+
+-- exercice 8
+SELECT s.ID_S, s.CITY, p.ID_P, p.CITY, j.ID_J, j.CITY
+FROM s,p,j
+WHERE s.CITY <> p.CITY
+  AND s.CITY <> j.CITY
+  AND p.CITY <> j.CITY;
+
+-- exercice 9
+SELECT DISTINCT commande1.ID_S
+FROM SPJ commande1
+    JOIN SPJ commande2 ON commande1.ID_S =  commande2.ID_S AND commande1.ID_P =  commande2.ID_P
+WHERE commande1.ID_J <> commande2.ID_J;
